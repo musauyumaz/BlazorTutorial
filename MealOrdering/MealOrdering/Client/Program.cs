7fd57de237045cb0be99d32c8a928e0c91448b34
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2024-02-20 21:34:48 +0300

    Blazor Tutorial | Blazor WebAssembly | İlk Uygulama

---
+++ MealOrdering/MealOrdering/Client/Program.cs
@@ -0,0 +1,14 @@
+using MealOrdering.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("MealOrdering.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("MealOrdering.ServerAPI"));
+
+await builder.Build().RunAsync();
