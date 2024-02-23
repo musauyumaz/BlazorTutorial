afa2dc9cb9b125f08898d2831a44ef3fe08a40bc
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2024-02-24 00:19:21 +0300

    Blazored Modal | Blazor Popup Component | Custom Popup

--- MealOrdering/MealOrdering.Client/Program.cs
+++ MealOrdering/MealOrdering.Client/Program.cs
@@ -1,11 +1,20 @@
 using MealOrdering.Client;
+using MealOrdering.Client.Utils;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddRadzenComponents();
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<ContextMenuService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ModalManager>();
 
 await builder.Build().RunAsync();
---
+++ MealOrdering/MealOrdering.Client/Utils/ModalManager.cs
@@ -0,0 +1,31 @@
+﻿using MealOrdering.Client.CustomComponents.Modals;
+using Radzen;
+
+namespace MealOrdering.Client.Utils
+{
+    public class ModalManager(DialogService _dialogService)
+    {
+        public async Task ShowMessageAsync(string title, string message, int duration = 0)
+        {
+            Dictionary<string, object>? dictionary = new();
+            dictionary.Add("Message", message);
+
+            await _dialogService.OpenAsync<ShowMessagePopupComponent>(title, dictionary);
+
+            if (duration > 0)
+            {
+                await Task.Delay(duration);
+                _dialogService.Close(true);
+            }
+
+        }
+
+        public async Task<bool> ConfirmationAsync(string title, string message)
+        {
+            Dictionary<string, object>? dictionary = new();
+            dictionary.Add("Message", message);
+            bool? modalResult = await _dialogService.Confirm(message, title);
+            return modalResult ?? false;
+        }
+    }
+}
